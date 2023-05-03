f209309f0c56e2da5d619ea93962ac5ca04d0a11
TarunNair445 <132118036+TarunNair445@users.noreply.github.com>
2023-05-03 20:51:08 +0530

    Add files via upload

--- python1.py
+++ python1.py
@@ -19,4 +19,21 @@ word1 = input('Enter the word you want to replace: ')
 word2 = input('enter the word you want to replace it with: ')
 print(sentence.replace(word1,word2))
 
-                    
+list1 = [1,2,3,4,5]
+list2 = ['Tarun ', 'Tushar','Nakul','valorant','Pubg']
+list1.extend(list2)
+print(list1)
+
+
+list1 = [2,7,3,4,5]
+list2 = ['Tarun ', 'Tushar','Nakul','valorant','Pubg']
+list1.sort()
+print(list1)    
+
+def greetings_function(name):
+    print('WELCOME'+name)
+    
+greetings_function(' Tarun Nair')    
+    
+    
+    
