70f6e5d20fdf9db4f18e812c369caf5a554670b4
TarunNair445 <132118036+TarunNair445@users.noreply.github.com>
2023-05-07 21:55:28 +0530

    Add files via upload

--- python3.py
+++ python3.py
@@ -21,3 +21,17 @@ for files in count_file.readline():
     print(files) 
 count_file.close() 
         
+count_file = open("country.txt",'w')
+count_file.write("new text here")
+   
+class person:
+    def __init__(self,name, age):
+        self.name =name
+        self.age = age
+name = input('enter the name:')
+age = input('enter the age:')
+
+p1 = person(name,age)
+print(p1.name)
+print(p1.age)                                       
+
